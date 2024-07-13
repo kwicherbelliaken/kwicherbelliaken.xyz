8ff3f0cdfebed3b8fa3947b7f31d537656b3cc52
slackermorris <morris.w.jack@gmail.com>
2024-07-14 11:52:07 +1200

    feature: modify sst config to point to production domain

--- sst.config.ts
+++ sst.config.ts
@@ -6,14 +6,20 @@ export default {
 	config(_input) {
 		return {
 			name: "kwicherbelliaken.xyz",
-			region: "us-east-1",
+			region: "ap-southeast-2",
 		};
 	},
 	stacks(app) {
 		app.stack(function Site({ stack }) {
-			const site = new AstroSite(stack, "site");
+			const site = new AstroSite(stack, "site", {
+				customDomain: {
+					domainAlias: "www.kwicherbelliaken.studio",
+					domainName: app.stage === "production" ? "kwicherbelliaken.studio" : undefined,
+				},
+			});
+
 			stack.addOutputs({
-				url: site.url,
+				url: site.customDomainUrl || site.url,
 			});
 		});
 	},
