a69f8ce293d588f701d590c3004543ed3de221bb
slackermorris <morris.w.jack@gmail.com>
2024-10-06 15:21:28 +1300

    feature: update code theme

--- src/site.config.ts
+++ src/site.config.ts
@@ -64,13 +64,13 @@ export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
 		// generate theme CSS selectors compatible with cactus-theme dark mode switch
 		if (styleVariants.length >= 2) {
 			const baseTheme = styleVariants[0]?.theme;
-			const altTheme = styleVariants.find((v) => v.theme.type !== baseTheme?.type)?.theme;
+			const altTheme = styleVariants.find((v) => v.theme.type !== baseTheme.type)?.theme;
 			if (theme === baseTheme || theme === altTheme) return `[data-theme='${theme.type}']`;
 		}
 		// return default selector
 		return `[data-theme="${theme.name}"]`;
 	},
 	// One dark, one light theme => https://expressive-code.com/guides/themes/#available-themes
-	themes: ["dracula", "github-light"],
+	themes: ["dracula", "catppuccin-mocha"],
 	useThemedScrollbars: false,
 };
