1580dd5e3e6e44ee0bb82aa2f2e7f5e5fd26fe43
slackermorris <morris.w.jack@gmail.com>
2025-03-16 10:58:28 +1300

    refactor: use expressive code with markdoc, needed custom code component

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,6 +1,34 @@
 import { component, defineMarkdocConfig, nodes } from "@astrojs/markdoc/config";
 
 export default defineMarkdocConfig({
+	nodes: {
+		fence: {
+			attributes: {
+				...nodes.fence.attributes,
+				collapse: {
+					required: false,
+					type: String,
+				},
+				content: { required: true, type: String },
+				frame: { matches: ["auto", "none", "code", "terminal"], type: String },
+				language: { type: String },
+				meta: {
+					required: false,
+					type: String,
+				},
+				showLineNumbers: {
+					required: false,
+					type: Boolean,
+				},
+				startLineNumber: {
+					required: false,
+					type: Number,
+				},
+				title: { type: String },
+			},
+			render: component("./src/components/CodeBlock.astro"),
+		},
+	},
 	tags: {
 		image: {
 			attributes: {
